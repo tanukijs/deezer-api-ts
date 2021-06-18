98fc351c4acc6333b13c6a35010058ad283e41f3
Tanuki <tanuki.contact@gmail.com>
2021-06-18 21:09:36 +0200

    ✨ search users

---
+++ src/search/user/interfaces/User.ts
@@ -0,0 +1,11 @@
+export interface UserInterface {
+  id: string
+  name: string
+  picture: string
+  picture_small: string
+  picture_medium: string
+  picture_big: string
+  picture_xl: string
+  tracklist: string
+  type: string
+}
