d52c65c2212be887709a64fdc6a838274ebe9e17
Tanuki <tanuki.contact@gmail.com>
2021-06-29 13:08:22 +0200

    ✨ album.fans

--- src/repositories/album.repository.ts
+++ src/repositories/album.repository.ts
@@ -1,6 +1,7 @@
 import got from 'got'
 import { AlbumResponse } from '../responses/album.response'
 import { AlbumTracksResponse } from '../responses/album-tracks.response'
+import { AlbumFansResponse } from '../responses/album-fans.response'
 
 export function getAlbum(albumId: number): Promise<AlbumResponse> {
   return got(`https://api.deezer.com/album/${albumId}`).json()
@@ -8,4 +9,8 @@ export function getAlbum(albumId: number): Promise<AlbumResponse> {
 
 export function getAlbumTracks(albumId: number): Promise<AlbumTracksResponse> {
   return got(`https://api.deezer.com/album/${albumId}/tracks`).json()
+}
+
+export function getAlbumFans(albumId: number): Promise<AlbumFansResponse> {
+  return got(`https://api.deezer.com/album/${albumId}/fans`).json()
 }
---
+++ src/responses/album-fans.response.ts
@@ -0,0 +1,12 @@
+export interface AlbumFansResponse {
+  id: number
+  name: string
+  link: string
+  picture: string
+  picture_small: string
+  picture_medium: string
+  picture_big: string
+  picture_xl: string
+  tracklist: string
+  type: string
+}
