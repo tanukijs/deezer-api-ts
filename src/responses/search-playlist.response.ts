adda693336dd195d839581daefcdfaa88f56e169
Tanuki <tanuki.contact@gmail.com>
2021-06-29 13:13:11 +0200

    ♻️ removing comma

--- src/responses/search-playlist.response.ts
+++ src/responses/search-playlist.response.ts
@@ -1,26 +1,26 @@
 export interface SearchPlaylistUser {
-  id: number;
-  name: string;
-  tracklist: string;
-  type: string;
+  id: number
+  name: string
+  tracklist: string
+  type: string
 }
 
 export interface SearchPlaylistResponse {
-  id: number;
-  title: string;
-  public: boolean;
-  nb_tracks: number;
-  link: string;
-  picture: string;
-  picture_small: string;
-  picture_medium: string;
-  picture_big: string;
-  picture_xl: string;
-  checksum: string;
-  tracklist: string;
-  creation_date: string;
-  md5_image: string;
-  picture_type: string;
-  user: SearchPlaylistUser;
-  type: string;
+  id: number
+  title: string
+  public: boolean
+  nb_tracks: number
+  link: string
+  picture: string
+  picture_small: string
+  picture_medium: string
+  picture_big: string
+  picture_xl: string
+  checksum: string
+  tracklist: string
+  creation_date: string
+  md5_image: string
+  picture_type: string
+  user: SearchPlaylistUser
+  type: string
 }
